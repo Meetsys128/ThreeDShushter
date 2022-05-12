9e234f224f5f89a5c828d1a09bebc931289897ad
Meetsys16 <91048024+Meetsys128@users.noreply.github.com>
2022-05-12 13:08:59 +0200

    bn

--- DirectoryName/js/game.js
+++ DirectoryName/js/game.js
@@ -1,6 +1,6 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.117.1/build/three.module.js';
 
-console.log("game v1.1.4");
+console.log("game v1.1.5");
 
 let change = 0;
 let last = 0;
