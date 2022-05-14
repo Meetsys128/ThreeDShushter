c163cd54ea87a053306d35597f290e278950366c
Meetsys16 <91048024+Meetsys128@users.noreply.github.com>
2022-05-14 14:51:18 +0200

    Update game.js

--- DirectoryName/js/game.js
+++ DirectoryName/js/game.js
@@ -197,10 +197,10 @@ loader.load( './orange.ma', function ( gltf ) {
 } );
 */
 			const scene = new THREE.Scene();
-			const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+			const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight , 0.1, 1000 );
 			const renderer = new THREE.WebGLRenderer();
 		
-			renderer.setSize( window.innerWidth, window.innerHeight );
+			renderer.setSize( window.innerWidth - 10, window.innerHeight - 10 );
 			document.body.appendChild( renderer.domElement );
             
 			const loader = new THREE.TextureLoader();
